549632aa07115a939dc14e894db1ec79edc53b84
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-02-24 14:48:52 +0000

    Added exmaple with System.out.println and print

--- Hello.java
+++ Hello.java
@@ -1,7 +1,11 @@
 public class Hello {
 
   public static void main(String[] args) {
+    // println the cursor is moved to the next line
     System.out.println("Hello world!");
+    // print the cursor is remains on same
+    System.out.print("I am learning ");
+    System.out.print("Java");
   }
 
 }
