98568af2ee6ea1ce0f36544020e048c822b789a2
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-02-24 16:43:09 +0000

    Created MyProfile programme to explore variables using java

---
+++ MyProfile.java
@@ -0,0 +1,11 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "Chris";
+    int age = 40;
+    double desiredSalary = 500000.99;
+    char gender = 'm';
+    boolean lookingForJob = true;
+    String myProfile = "Name: " + name + "\nAge: " + age + "\nDesired Salary: " + desiredSalary + "\nGender: " + gender + "\nJob Hunting: " + lookingForJob;
+    System.out.println(myProfile);
+	}
+}
