8bbb5e74d6367ef5d1d5ba1d49b9c5d04e1d6dda
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-01 12:22:43 +0000

    Created Playlist class and Created a ArrayList that can hold String values

---
+++ Arrays-and-ArrayLists/Playlist.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class Playlist {
+  
+  public static void main(String[] args) {
+    // Created a ArrayList that can hold String values
+    ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
+
+  }
+  
+}
