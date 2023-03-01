7d1ca592db867aae33097bae84eeb4cb9e0c25c5
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-03-01 12:24:14 +0000

    Added songs to ArrayList in Playlist

--- Arrays-and-ArrayLists/Playlist.java
+++ Arrays-and-ArrayLists/Playlist.java
@@ -5,7 +5,13 @@ class Playlist {
   public static void main(String[] args) {
     // Created a ArrayList that can hold String values
     ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
-
+    // Add songs to ArrayList
+    desertIslandPlaylist.add("Yesterday");
+    desertIslandPlaylist.add("Hey Jude");
+    desertIslandPlaylist.add("Lady Madonna");
+    desertIslandPlaylist.add("Let It Be");
+    desertIslandPlaylist.add("Day Tripper");
+    desertIslandPlaylist.add("All You Need Is Love");
   }
   
 }
