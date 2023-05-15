e081629a02e4bc1769fc69c886267f679c61f3be
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-05-15 15:24:31 +0100

    Used Lambda expression to sort words by length

--- Lambda-Expressions/lambda.java
+++ Lambda-Expressions/lambda.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Lambda {
 
   public static void main(String[] args) {
+
+    // Prints square route for each item using Lambda expression 
     List<Integer> items = new ArrayList<>();
     items.add(1);
     items.add(2);
@@ -11,5 +14,11 @@ class Lambda {
     items.add(4);
 
     items.forEach(item -> System.out.println(item*item));
+
+    // Prints words from shortest to longest
+
+    String[] words = {"Bye", "Hi", "Adios", "Hola", "Aloha"};
+    Arrays.sort(words, (s1, s2) -> Integer.compare(s1.length(), s2.length()));
+    System.out.println(Arrays.toString(words));
   }
 }
